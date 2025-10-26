0098f7e8196f6e82b5d4523789c3d2b29c2544b9
hassan <hk5708102@gmail.com>
2025-10-26 03:36:11 +0300

    Initial commit

---
+++ lib/item_model.dart
@@ -0,0 +1,9 @@
+class ItemModel
+{
+ final String name ;
+ final String img;
+ final String value;
+  bool accept  ;
+
+  ItemModel({ required this.value,required this.name,required this.img,this.accept= false } );
+}
