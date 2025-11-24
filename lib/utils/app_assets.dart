4b82a48c8575451eafc7228d3af8786aed41ffd5
hassan <hk5708102@gmail.com>
2025-11-24 16:44:10 +0200

    polishing

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,15 @@
+
+class AppAssets {
+  AppAssets._();
+
+  static const String lionImage = 'assets/images/lion.png';
+  static const String pandaImage = 'assets/images/panda.png';
+  static const String camelImage = 'assets/images/camel.png';
+  static const String dogImage = 'assets/images/dog.png';
+  static const String catImage ='assets/images/cat.png';
+  static const String horseImage = 'assets/images/horse.png';
+  static const String sheepImage = 'assets/images/sheep.png';
+  static const String foxImage = 'assets/images/fox.png';
+  static const String cowImage = 'assets/images/cow.png';
+  static const String henImage = 'assets/images/hen.png';
+}
